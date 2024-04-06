e70dd223a1b4fa2639d27dbb7687c5e74cb90d8e
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-04-06 16:31:13 -0300

    initial style created

--- server-side/server.js
+++ server-side/server.js
@@ -1,7 +1,22 @@
 const http = require('http')
+const fs = require('fs')
 
 const app = http.createServer((req, res) => {
     console.log('Someone is trying to acess: '.concat(req.url))
+
+    let path = '../client-side/'
+
+    switch (req.url) {
+        case '/style.css':
+            path+='style.css'
+            break
+        case '/':
+        default:
+            path+='index.html'
+            break
+    }
+    res.end(fs.readFileSync(path))
+
 })
 
 app.listen(3000, () => {
